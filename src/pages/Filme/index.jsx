5f21f63268d238e6a0e4724a829650b8cb8c970b
HerbertonMiguel-Dev <herbertonmiguel.dev@gmail.com>
2024-09-05 16:25:59 -0300

    criado button trailer, importado useNavigate e toastify

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
-import { useParams, seNavigate } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import './filme-info.css';
-
 import api from '../../services/api';
+import { toast } from 'react-toastify'
 
 function Filme(){
   const { id } = useParams();
@@ -46,13 +46,13 @@ function Filme(){
     const hasFilme = filmesSalvos.some( (filmesSalvo) => filmesSalvo.id === filme.id)
 
     if(hasFilme){
-      alert("ESSE FILME JÁ ESTA NA LISTA");
+      toast.warn("Esse filme já está na sua lista!")
       return;
     }
 
     filmesSalvos.push(filme);
     localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
-    alert("FILME SALVO COM SUCESSO")
+    toast.success("Filme salvo com sucesso!")
 
   }
 
